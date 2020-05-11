6f9f8636326f8d7f1121df78e5dcf6ea77409088
Greg <gregoryc@outlook.com>
2020-05-10 23:43:02 -0700

    formatting changes from prettier

--- src/server/simulate.js
+++ src/server/simulate.js
@@ -96,16 +96,10 @@ function simulateOneIteration(
   }
 }
 
-
-
-
 /*
 Simulate the worst possible season for a player (naively, not following match schedule)
 */
-function simulateWorstSeasonForPlayer(
-  division,
-  targetPlayer
-) {
+function simulateWorstSeasonForPlayer(division, targetPlayer) {
   const cloneDivision = JSON.parse(JSON.stringify(division));
   const divisionStandings = cloneDivision.standings;
 
@@ -114,27 +108,26 @@ function simulateWorstSeasonForPlayer(
   for (let p = 0; p < divisionStandings.length; p++) {
     loopPlayer = divisionStandings[p];
     // Worst case for target player
-    if (loopPlayer.name == targetPlayer.name){
+    if (loopPlayer.name == targetPlayer.name) {
       const toPlay = matchesPerSeason - loopPlayer.mp;
       loopPlayer.points += 0; // 0s for clarity
       loopPlayer.gd += toPlay * -3;
       loopPlayer.gf += 0;
-      loopPlayer.ga += toPlay * 3; 
+      loopPlayer.ga += toPlay * 3;
       loopPlayer.losses += toPlay;
       loopPlayer.wins += 0;
       loopPlayer.mp += toPlay;
     } else {
       // Best case for all others
-    const toPlay = matchesPerSeason - loopPlayer.mp;
-    loopPlayer.points += toPlay * 4;
-    loopPlayer.gd += toPlay * 3;
-    loopPlayer.gf += toPlay * 3;
-    loopPlayer.ga += 0; // for clarity
-    loopPlayer.losses += 0; // for clarity
-    loopPlayer.wins += toPlay;
-    loopPlayer.mp += toPlay;
+      const toPlay = matchesPerSeason - loopPlayer.mp;
+      loopPlayer.points += toPlay * 4;
+      loopPlayer.gd += toPlay * 3;
+      loopPlayer.gf += toPlay * 3;
+      loopPlayer.ga += 0; // for clarity
+      loopPlayer.losses += 0; // for clarity
+      loopPlayer.wins += toPlay;
+      loopPlayer.mp += toPlay;
     }
-    
   }
 
   // Sort the simulated results by points, GD, etc.
@@ -142,14 +135,10 @@ function simulateWorstSeasonForPlayer(
   return divisionStandings;
 }
 
-
 /*
 Simulate the worst possible season for a player (naively, not following match schedule)
 */
-function simulateBestSeasonForPlayer(
-  division,
-  targetPlayer
-) {
+function simulateBestSeasonForPlayer(division, targetPlayer) {
   const cloneDivision = JSON.parse(JSON.stringify(division));
   const divisionStandings = cloneDivision.standings;
 
@@ -158,25 +147,25 @@ function simulateBestSeasonForPlayer(
   for (let p = 0; p < divisionStandings.length; p++) {
     loopPlayer = divisionStandings[p];
     // Worst case for all other players
-    if (loopPlayer.name != targetPlayer.name){
+    if (loopPlayer.name != targetPlayer.name) {
       const toPlay = matchesPerSeason - loopPlayer.mp;
       loopPlayer.points += 0; // 0s for clarity
       loopPlayer.gd += toPlay * -3;
       loopPlayer.gf += 0;
-      loopPlayer.ga += toPlay * 3; 
+      loopPlayer.ga += toPlay * 3;
       loopPlayer.losses += toPlay;
       loopPlayer.wins += 0;
       loopPlayer.mp += toPlay;
     } else {
       // Best case for target player
-    const toPlay = matchesPerSeason - loopPlayer.mp;
-    loopPlayer.points += toPlay * 4;
-    loopPlayer.gd += toPlay * 3;
-    loopPlayer.gf += toPlay * 3;
-    loopPlayer.ga += 0; // for clarity
-    loopPlayer.losses += 0; // for clarity
-    loopPlayer.wins += toPlay;
-    loopPlayer.mp += toPlay;
+      const toPlay = matchesPerSeason - loopPlayer.mp;
+      loopPlayer.points += toPlay * 4;
+      loopPlayer.gd += toPlay * 3;
+      loopPlayer.gf += toPlay * 3;
+      loopPlayer.ga += 0; // for clarity
+      loopPlayer.losses += 0; // for clarity
+      loopPlayer.wins += toPlay;
+      loopPlayer.mp += toPlay;
     }
   }
 
@@ -185,21 +174,24 @@ function simulateBestSeasonForPlayer(
   return divisionStandings;
 }
 
-function didClinchPromo(targetPlayer, division){
-  const divisionStandings = simulateWorstSeasonForPlayer(division, targetPlayer);
+function didClinchPromo(targetPlayer, division) {
+  const divisionStandings = simulateWorstSeasonForPlayer(
+    division,
+    targetPlayer
+  );
   let promoed = false;
 
   // Check if the guaranteed promoed players includes the target
   for (let i = 0; i < division.numAutoPromo + division.numWinner; i++) {
     const promoedName = divisionStandings[i].name;
-    if (promoedName === targetPlayer.name){
+    if (promoedName === targetPlayer.name) {
       promoed = true;
     }
   }
   return promoed;
 }
 
-function didClinchNonRelegation(player, division){
+function didClinchNonRelegation(player, division) {
   const divisionStandings = simulateWorstSeasonForPlayer(division, player);
   let relegated = false;
 
@@ -208,14 +200,14 @@ function didClinchNonRelegation(player, division){
   const numRelegate = division.numSoftRelegate + division.numHardRelegate;
   for (let j = end; j > end - numRelegate; j--) {
     const relegatedName = divisionStandings[j].name;
-    if (relegatedName === player.name){
+    if (relegatedName === player.name) {
       relegated = true;
     }
   }
   return !relegated;
 }
 
-function didClinchRelegation(player, division){
+function didClinchRelegation(player, division) {
   const divisionStandings = simulateBestSeasonForPlayer(division, player);
   let relegated = false;
   console.log("Best:", divisionStandings);
@@ -224,35 +216,36 @@ function didClinchRelegation(player, division){
   const end = division.players.length - 1;
   for (let j = end; j > end - division.numHardRelegate; j--) {
     const relegatedName = divisionStandings[j].name;
-    if (relegatedName === player.name){
+    if (relegatedName === player.name) {
       relegated = true;
     }
   }
   return relegated;
 }
 
-function didClinchNonPromo(player, division){
+function didClinchNonPromo(player, division) {
   const divisionStandings = simulateBestSeasonForPlayer(division, player);
   let promoed = false;
 
   // Auto promo or win
-  for (let i = 0; i < division.numAutoPromo + division.numWinner + division.numSoftPromo; i++) {
+  for (
+    let i = 0;
+    i < division.numAutoPromo + division.numWinner + division.numSoftPromo;
+    i++
+  ) {
     const promoedName = divisionStandings[i].name;
-    if (promoedName === player.name){
+    if (promoedName === player.name) {
       promoed = true;
     }
   }
   return !promoed;
 }
 
-
-
 // const memeDivision = util.memeDivisionData[0]
 // console.log("clinchedPromo:", didClinchPromo(memeDivision.standings[0], memeDivision))
 // console.log("clinchedRelegation:", didClinchRelegation(memeDivision.standings[0], memeDivision))
 // console.log("clinchedNonPromo:", didClinchNonPromo(memeDivision.standings[0], memeDivision))
 // console.log("clinchedNonRelegation:", didClinchNonRelegation(memeDivision.standings[0], memeDivision))
-
 
 /* 
 Main method to:
@@ -291,14 +284,23 @@ function runSimulation(division, matchSchedule) {
     ).toFixed(3);
 
     // Check 100s and 0s for if they actually clinched or not
-    if (player.promoChance == 100 && !didClinchPromo(player, division)){
+    if (player.promoChance == 100 && !didClinchPromo(player, division)) {
       player.promoChance = 99.99;
-    } else if (player.promoChance == 0 && !didClinchNonPromo(player, division)){
+    } else if (
+      player.promoChance == 0 &&
+      !didClinchNonPromo(player, division)
+    ) {
       player.promoChance = 0.01;
     }
-    if (player.relegationChance == 100 && !didClinchRelegation(player, division)){
+    if (
+      player.relegationChance == 100 &&
+      !didClinchRelegation(player, division)
+    ) {
       player.relegationChance = 99.99;
-    } else if (player.relegationChance == 0 && !didClinchNonRelegation(player, division)){
+    } else if (
+      player.relegationChance == 0 &&
+      !didClinchNonRelegation(player, division)
+    ) {
       player.relegationChance = 0.01;
     }
   }
