33960fe83e9041440dcf037ef6a283c40a6235c5
tugi1220 <74648091+tugi1220@users.noreply.github.com>
2020-11-18 09:13:56 +0100

    Update config_data.js

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -271,7 +271,7 @@ const divisionData = [
   }
 ];
 
-const adminRole = ["GregBoomCannon#4087", "moodeuce#8573", "Kingsman#0364"];
+const adminRole = ["GregBoomCannon#4087", "moodeuce#8573", "Kingsman#0364", "Tugi#7004"];
 
 const restreamerRole = [
   "moodeuce#8573",
