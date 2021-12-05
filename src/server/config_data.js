8477b064dc5a14e89ec50017a8070869fbe845d9
moodeuce <62537287+moodeuce@users.noreply.github.com>
2021-12-06 07:34:41 +0800

    Update config_data.js

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -548,8 +548,7 @@ const restreamerRole = [
   "Fidhapre#9453",
   "KunfusingPoke#6098",
   "Twifty#3688",
-  "UnOr1ginalName#8655",
-  "TKK24#2514"
+  "UnOr1ginalName#8655"
 ];
 
 module.exports = {
