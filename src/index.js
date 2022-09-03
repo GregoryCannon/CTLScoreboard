12c9ec836c965561a64f1a240a3d264195cc00a2
Nick Woods <nickwoods@Nicks-MacBook-Pro.local>
2022-09-03 08:11:37 -0700

    perform redirect w js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./client/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+window.location.replace('https://tnp.tetris.lol');
