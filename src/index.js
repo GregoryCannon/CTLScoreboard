3ec91edd00e8daf66c4cf92223163810119276ef
Nick Woods <nickwoods@Nicks-MacBook-Pro.local>
2022-09-03 08:16:12 -0700

    Revert "perform redirect w js"
    This reverts commit 12c9ec836c965561a64f1a240a3d264195cc00a2.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./client/App";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-window.location.replace('https://tnp.tetris.lol');
+ReactDOM.render(<App />, document.getElementById("root"));
