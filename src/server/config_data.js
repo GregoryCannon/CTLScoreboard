75d7f7a4010272eb168ee27e9dc424048752da42
Nick Woods <14417375+nawoods@users.noreply.github.com>
2023-01-27 15:27:51 -0800

    Change benfamir's discord tag

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -586,7 +586,7 @@ const restreamerRole = [
   "doodle#3414",
   "Allenbot#1138",
   "liolio1230#9656",
-  "Benfamir#0001",
+  "Benfamir#6783",
   "DanV#0061",
   "Bendy1#6609",
   "Chris_Moulistanos#3251",
