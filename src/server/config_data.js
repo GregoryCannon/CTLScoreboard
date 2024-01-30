ffec7659a8d3bb8197d9423ad70c59701dff9f4b
Nick Woods <14417375+nawoods@users.noreply.github.com>
2024-01-29 21:27:59 -0800

    Add Bronze 34

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -338,6 +338,26 @@ const divisionData = [
       "ethanthoughts"
     ],
   },
+  {
+    divisionName: "Bronze 34",
+    competition: "tnp",
+    oneMatchPerPair: false,
+    numWinner: 0,
+    numAutoPromo: 1,
+    numPlayoffPromo: 1,
+    numPlayoffRelegate: 0,
+    numAutoRelegate: 0,
+    players: [
+      "yarno",
+      "tobs",
+      "ian w",
+      "ethanthoughts",
+      "saymyname",
+      "hiphyemac",
+      "falkite",
+      "komi"
+    ],
+  },
 
   {
     divisionName: "Beginner 21",
