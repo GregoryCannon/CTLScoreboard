cc81322df362ca7823b4808b2ffe58585ee4a254
RichardWolfVI <juanchito2006@gmail.com>
2024-07-28 23:50:38 -0500

    Offloaded Bronze 47

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -323,23 +323,6 @@ const divisionData = [
   },
 
   {
-    divisionName: "Bronze 47",
-    competition: "tnp",
-    oneMatchPerPair: false,
-    numWinner: 0,
-    numAutoPromo: 1,
-    numPlayoffPromo: 1,
-    numPlayoffRelegate: 0,
-    numAutoRelegate: 0,
-    players: [
-      "mansion",
-      "temphuibis", 
-      "letcreate",
-      "adamirish", 
-      "a_benz"
-    ]
-  },
-  {
     divisionName: "Bronze 48",
     competition: "tnp",
     oneMatchPerPair: false,
