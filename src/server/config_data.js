5dec886f263e96f0835e334ba4086bbfe89c100b
RichardWolfVI <juanchito2006@gmail.com>
2024-07-28 23:46:42 -0500

    Offloaded Silver 42

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -268,26 +268,6 @@ const divisionData = [
   },
 
   {
-    divisionName: "Silver 42",
-    competition: "tnp",
-    oneMatchPerPair: false,
-    numWinner: 0,
-    numAutoPromo: 1,
-    numPlayoffPromo: 1,
-    numPlayoffRelegate: 1,
-    numAutoRelegate: 0,
-    players: [
-      "enscalight", 
-      "snowytetris", 
-      "coverdales", 
-      "mathwiz", 
-      "arbaro",
-      "vasco",
-      "lutet",
-      "rory"
-    ]
-  },
-  {
     divisionName: "Silver 43",
     competition: "tnp",
     oneMatchPerPair: false,
