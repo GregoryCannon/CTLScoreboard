2ccfffaa0ef2727a785b949aead3ccd4dd3bf228
RichardWolfVI <juanchito2006@gmail.com>
2024-10-24 23:03:48 -0500

    Offloaded Gold 40

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -232,24 +232,6 @@ const divisionData = [
   },
 
   {
-    divisionName: "Gold 40",
-    competition: "tnp",
-    oneMatchPerPair: false,
-    numWinner: 0,
-    numAutoPromo: 1,
-    numPlayoffPromo: 1,
-    numPlayoffRelegate: 1,
-    numAutoRelegate: 0,
-    players: [
-      "bmac",
-      "marts",
-      "rory",
-      "arbaro", 
-      "mathwiz",
-      "pohonjati",
-    ]
-  },
-  {
     divisionName: "Gold 41",
     competition: "tnp",
     oneMatchPerPair: false,
