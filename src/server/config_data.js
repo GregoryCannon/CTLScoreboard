4f2760de25f142ce9e0b6298a71759a45407b23f
moodeuce <62537287+moodeuce@users.noreply.github.com>
2026-03-04 12:48:48 +0800

    Update config_data.js

--- src/server/config_data.js
+++ src/server/config_data.js
@@ -26,7 +26,7 @@ const divisionData = [
     oneMatchPerPair: true,
     maxPointsPerMatch: 7,
     numWinner: 1,
-    numPrizeMoney: 2,
+    numPrizeMoney: 4,
     numAutoPromo: 0,
     numPlayoffPromo: 0,
     numPlayoffRelegate: 1,
